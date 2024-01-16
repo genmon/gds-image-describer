86f2553f2ce4081471e12b75e10029f155d9ada9
Matt Webb <matt@interconnected.org>
2024-01-16 11:28:59 +0000

    Extract images

--- party/vision.ts
+++ party/vision.ts
@@ -5,12 +5,29 @@ export default class VisionServer implements Party.Server {
 
   async onRequest(req: Party.Request) {
     if (req.method === "POST") {
+      const data = (await req.json()) as any;
       const response = {
-        images: ["test URL 1", "test URL 2"],
+        images: await this.fetchImageUrls(data.url),
       };
       return Response.json(response);
     }
 
     return new Response("Method Not Allowed", { status: 405 });
+  }
+
+  async fetchImageUrls(url: string) {
+    const response = await fetch(url);
+    const html = await response.text();
+
+    // This is awful but use regexps to get img.src
+    // Only inside div.entry-content
+    const imgSrcRegex = /<img.*?src="(.*?)"/g;
+    const matches = html.matchAll(imgSrcRegex);
+    const urls = [];
+    for (const match of matches) {
+      urls.push(match[1]);
+    }
+
+    return urls;
   }
 }
